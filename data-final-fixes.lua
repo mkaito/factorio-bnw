c199027b4269c33cbd773f44ca57bfb8e5415ed5
Michishige Kaito <me@mkaito.com>
2019-04-26 16:17:54 +0100

    Update sane steel

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -59,7 +59,9 @@ data.raw["roboport"]["roboport"].construction_radius = 64 -- default: 54 -- 2c
 data.raw["roboport"]["roboport"].logistics_radius = 32 -- default: 28 -- 1c
 
 -- Sane steel production
-data.raw.recipe["steel-plate"].normal.ingredients[1][2] = 4
-data.raw.recipe["steel-plate"].normal.energy_required = 14
-data.raw.recipe["steel-plate"].expensive.ingredients[1][2] = 8
-data.raw.recipe["steel-plate"].expensive.energy_required = 28
+-- iron:steel 5:1 -> 4:1
+-- Note: 1 iron plate needs 3.2 energy
+data.raw.recipe["steel-plate"].normal.ingredients[1][2] = 4 -- default: 5
+data.raw.recipe["steel-plate"].normal.energy_required = 12.8 -- default: 16
+data.raw.recipe["steel-plate"].expensive.ingredients[1][2] = 8 -- default: 10
+data.raw.recipe["steel-plate"].expensive.energy_required = 25.6 -- default: 32
