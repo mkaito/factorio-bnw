4e451c93cea50c53fa3f64c8358919bb858818ef
Michishige Kaito <me@mkaito.com>
2019-04-26 16:17:33 +0100

    Chunkify power poles and roboports

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -42,12 +42,21 @@
 -- data.raw.item["logistic-robot"].stack_size = 200
 
 -- Chunky power poles
-data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = 32
-data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance = 16
+data.raw["electric-pole"]["big-electric-pole"].maximum_wire_distance = 32 -- default: 30 -- 1c
+data.raw["electric-pole"]["big-electric-pole"].supply_area_distance = 2 -- default: 2 -- c/16
+
+data.raw["electric-pole"]["medium-electric-pole"].maximum_wire_distance = 12 -- default: 9 -- c/3
+data.raw["electric-pole"]["medium-electric-pole"].supply_area_distance = 4.5 -- default: 3.5 -- c/8+.5 (from center of pole)
+
+data.raw["electric-pole"]["small-electric-pole"].maximum_wire_distance = 8 -- default: 7.5 -- c/4
+data.raw["electric-pole"]["small-electric-pole"].supply_area_distance = 2.5 -- default: 2.5 -- c/16+.5 (from center of pole)
+
+data.raw["electric-pole"]["substation"].maximum_wire_distance = 24 -- default: 18 -- 2c/3
+data.raw["electric-pole"]["substation"].supply_area_distance = 12 -- default: 9 -- c/3
 
 -- Chunky roboports
-data.raw["roboport"]["roboport"].construction_radius = 64
-data.raw["roboport"]["roboport"].logistics_radius = 32
+data.raw["roboport"]["roboport"].construction_radius = 64 -- default: 54 -- 2c
+data.raw["roboport"]["roboport"].logistics_radius = 32 -- default: 28 -- 1c
 
 -- Sane steel production
 data.raw.recipe["steel-plate"].normal.ingredients[1][2] = 4
